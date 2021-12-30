5d2a9d4e0b095b040d57f776648a293e404d749c
malibayram91 <malibayram91@gmail.com>
2021-12-30 15:04:58 +0300

    login and registeration completed

--- app/src/main/java/come491/hanibana/Screen/home/home.java
+++ app/src/main/java/come491/hanibana/Screen/home/home.java
@@ -6,13 +6,18 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -23,12 +28,16 @@ import java.util.ArrayList;
 
 import come491.hanibana.Adapter.categoryAdapter;
 import come491.hanibana.Adapter.productAdapter;
+import come491.hanibana.LoginActivity;
 import come491.hanibana.Model.categoryModel;
 import come491.hanibana.Model.productModel;
 import come491.hanibana.R;
 
+import androidx.appcompat.widget.Toolbar;
+
 public class home extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
 
     // ürünlerimizi yerleştirecegimiz listemiz
     private RecyclerView categoryList;
@@ -40,7 +49,7 @@ public class home extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;
 
-    private void init(){
+    private void init() {
         // arayüzdeki kompanente ulaşmak için id sini kullanıyoruz
         categoryList = findViewById(R.id.categoryList);
 
@@ -54,11 +63,11 @@ public class home extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-                if(item.toString().equals("Favorite")){
+                if (item.toString().equals("Favorite")) {
 
                 }
 
-                Log.d("seçilen",item.toString());
+                Log.d("seçilen", item.toString());
                 return false;
             }
         });
@@ -73,11 +82,41 @@ public class home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        // Find the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
+
+        mAuth = FirebaseAuth.getInstance();
+
         init();
         loadCategory();
     }
 
-    private void loadCategory(){
+    // Menu icons are inflated just as they were with actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.miLogout) {
+            mAuth.signOut();
+
+            Intent intent = new Intent(home.this, LoginActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void loadCategory() {
         // bu aldıgımız referansa bir sürekli dinleyici atıyoruz
         reference.addValueEventListener(new ValueEventListener() {
             @Override
